3d3803ad009b7e88454055674f872471e06016fe
Chris Foster <wordpressninja@me.com>
2017-10-08 21:40:11 -0500

    Create TodosController, Created TodosTableSeeder, Modified DatabaseSeeder & UserFactory for  information generator - Section 3 Lesson 10

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,3 +23,11 @@ $factory->define(App\User::class, function (Faker $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\ToDo::class, function (Faker $faker) {
+	return[
+		'todo' => $faker->sentence(10),
+	];
+
+
+});
